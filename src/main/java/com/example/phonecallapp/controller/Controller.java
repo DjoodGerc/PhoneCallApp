298aef18cd1a3a1782108b3051cf873b27a043c5
chmo_kudryavoe <avramenko.pav@yandex.ru>
2023-03-16 23:24:41 +0300

    rabbit mq added

--- src/main/java/com/example/phonecallapp/controller/Controller.java
+++ src/main/java/com/example/phonecallapp/controller/Controller.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,29 +15,28 @@ public class Controller {
     private DataAccessObject data;
 
     @PostMapping(value = "/saveCall")
-    public ResponseEntity<?> create(@RequestBody CallModel call){
-        DataAccessObject.   setField(call);
+    public ResponseEntity<?> create(@RequestBody CallModel call) {
+        DataAccessObject.setField(call);
         return new ResponseEntity<>(HttpStatus.CREATED);
 
     }
 
     @GetMapping(value = "/Calls")
-    public ResponseEntity<List<CallModel>> read(){
-        final List<CallModel> calls =DataAccessObject.getAllFields();
-        if (calls.isEmpty() && calls!=null){
+    public ResponseEntity<List<CallModel>> read() {
+        final List<CallModel> calls = DataAccessObject.getAllFields();
+        if (calls.isEmpty() && calls != null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        else{
-            return new ResponseEntity<>(calls,HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(calls, HttpStatus.OK);
         }
     }
+
     @GetMapping(value = "/Calls/{id}")
-    public ResponseEntity<CallModel> read(@PathVariable(name="id")int id){
-        final CallModel call =DataAccessObject.getFieldById(id);
-        if (call !=null){
-            return new ResponseEntity<>(call,HttpStatus.OK);
-        }
-        else{
+    public ResponseEntity<CallModel> read(@PathVariable(name = "id") int id) {
+        final CallModel call = DataAccessObject.getFieldById(id);
+        if (call != null) {
+            return new ResponseEntity<>(call, HttpStatus.OK);
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
--- src/main/java/com/example/phonecallapp/service/Service.java
+++
@@ -1,5 +0,0 @@
-package com.example.phonecallapp.service;
-
-public class Service {
-
-}
